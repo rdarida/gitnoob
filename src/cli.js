34d7f9fa5c21111aa82d7179fd8e1b6f705d2a20
rdarida <robertdarida86@gmail.com>
2020-10-29 01:31:19 +0100

    feat: implement add, set, and list functions

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
-const cli = require('.');
+const { gitnoob } = require('.');
 
-cli(process.cwd(), process.argv.slice(2));
+((cwd, argv) => {
+  try {
+    if (!argv.length) {
+      throw new Error("There isn't enough argument!");
+    }
+
+    gitnoob(cwd, argv[0], argv.slice(1));
+  } catch (error) {
+    console.log(error);
+  }
+
+  process.exit();
+})(process.cwd(), process.argv.slice(2));
--- src/index.js
+++ src/index.js
@@ -1,10 +1,98 @@
+const { resolve } = require('path');
+const { readJsonSync, writeJSONSync, existsSync } = require('fs-extra');
+const shell = require('shelljs');
+
+const GitNoobCmd = {
+  ADD: 'add',
+  SET: 'set',
+  LIST: 'list'
+};
+
+/**
+ * Checks the number of arguments.
+ * @param {string[]} argv Command line arguments.
+ * @param {number} need The number of arguments are needed.
+ */
+function checkArgv(argv, need, cmd) {
+  if (argv.length < need) {
+    throw new Error(`The "${cmd}" command needs ${need} argument(s)!`);
+  }
+}
+
+/**
+ * Adds new user to users.
+ * @param {string[]} argv
+ * @param {*} users
+ */
+function add(argv, users) {
+  checkArgv(argv, 2, 'add');
+  const [name, email] = argv;
+  users[name] = email;
+}
+
+/**
+ * Sets user to current git repository.
+ * @param {string[]} argv Command line arguments.
+ * @param {*} users A json object contains name & email pairs
+ * @param {string} cwd Current working directory.
+ */
+function set(argv, users, cwd) {
+  checkArgv(argv, 1, 'set');
+  const [name] = argv;
+
+  if (users[name] == undefined) {
+    throw new Error(`There isn't "${name}" user!`);
+  }
+
+  if (shell.which('git') && existsSync(resolve(cwd, '.git'))) {
+    shell.cd(cwd);
+    shell.exec(`git config user.name ${name}`);
+    shell.exec(`git config user.email ${users[name]}`);
+  }
+}
+
+/**
+ * Lists all saved users.
+ * @param {*} users A json object contains name & email pairs
+ */
+function list(users) {
+  console.log('List of users:');
+
+  for (const user in users) {
+    console.log('\t', user, users[user]);
+  }
+}
+
 /**
  * Runs gitnoob.
  * @param {string} cwd Current working directory.
  * @param {string[]} argv Command line arguments.
  */
-function gitnoob(cwd, argv) {
-  console.log('gitnoob', cwd, argv);
+function gitnoob(cwd, cmd, argv) {
+  const jsonPath = resolve(__dirname, '../gitnoob.json');
+  const json = readJsonSync(jsonPath);
+
+  switch (cmd) {
+    case GitNoobCmd.ADD:
+      add(argv, json.users);
+      break;
+
+    case GitNoobCmd.SET:
+      set(argv, json.users, cwd);
+      break;
+
+    case GitNoobCmd.LIST:
+      list(json.users);
+      break;
+
+    default:
+      throw new Error(`There is no "${cmd}" commmand!`);
+  }
+
+  writeJSONSync(jsonPath, json);
 }
 
-module.exports = gitnoob;
+module.exports = {
+  GitNoobCmd,
+  gitnoob
+};
